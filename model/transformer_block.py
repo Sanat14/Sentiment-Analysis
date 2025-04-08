05a84cc0fe89344aee1714b368f107989a5a68e5
Sanat <dusadsanat@gmail.com>
2025-04-08 23:19:34 +0100

    adding requirements

---
+++ model/transformer_block.py
@@ -0,0 +1,24 @@
+import tensorflow as tf
+from tensorflow.keras.layers import Layer, MultiHeadAttention, Dense, Dropout, LayerNormalization
+
+class TransformerBlock(Layer):
+    def __init__(self, embed_dim, num_heads, ff_dim, dropout_rate=0.1, **kwargs):
+        super(TransformerBlock, self).__init__(**kwargs)
+        self.att = MultiHeadAttention(num_heads=num_heads, key_dim=embed_dim)
+        self.ffn = tf.keras.Sequential([
+            Dense(ff_dim, activation='relu'),
+            Dense(embed_dim),
+        ])
+        self.dropout1 = Dropout(dropout_rate)
+        self.dropout2 = Dropout(dropout_rate)
+        self.layernorm1 = LayerNormalization(epsilon=1e-6)
+        self.layernorm2 = LayerNormalization(epsilon=1e-6)
+
+    def call(self, inputs, training=False):
+        attn_output = self.att(inputs, inputs)
+        attn_output = self.dropout1(attn_output, training=training)
+        out1 = self.layernorm1(inputs + attn_output)
+
+        ffn_output = self.ffn(out1)
+        ffn_output = self.dropout2(ffn_output, training=training)
+        return self.layernorm2(out1 + ffn_output)
